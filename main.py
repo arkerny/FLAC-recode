99ddb4828d8e200214d7ac203eace75239d74c7c
arkerny <arkerny@gmail.com>
2023-09-11 21:30:36 +0800

    edited main.py

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def trans(path):
     for allDir in pathDir:
         saveDir = allDir.replace('.flac', '.wav')
         saveDir = saveDir.replace('.mp3', '.wav')
-        cmd = "ffmpeg -i " + path + allDir + ' ' + path + saveDir
+        cmd = 'ffmpeg -i ' + path + allDir + ' ' + path + saveDir
         print(cmd)
-        # os.system(cmd)
+        try:
+            os.system(cmd)
+            print('Done')
+        except:
+            print('Fail!')
 
 if __name__ == '__main__':
     path = input('Please input dir path: ')
