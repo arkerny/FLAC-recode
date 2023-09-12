a3b7056581b4910787c058756408fea48a3a76f5
arkerny <arkerny@gmail.com>
2023-09-12 20:03:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import time
 
-def trans(path):
+def trans(path, savepath):
     pathDir = os.listdir(path)
     log = open('log.txt', 'w', encoding = 'utf-8')
 
@@ -12,7 +12,7 @@ def trans(path):
     for allDir in pathDir:
         saveDir = allDir.replace('.flac', '_recode.flac')
         saveDir = saveDir.replace('.mp3', '_recode.flac')
-        cmd = 'ffmpeg -y -i "' + path + allDir + '" "' + path + '00_' + saveDir + '"'
+        cmd = 'ffmpeg -y -i "' + path + allDir + '" "' + savepath + saveDir + '"'
         log.write('$ Time: ' + time.strftime("%a %b %d %H:%M %Y", time.localtime()) + '\n')
         log.write('# ' + cmd + ' Start!' + '\n')
         ret = subprocess.getoutput(cmd)
@@ -26,4 +26,5 @@ def trans(path):
 
 if __name__ == '__main__':
     path = input('Please input dir path: ')
-    trans(path)
+    savepath = input('Please input save path: ')
+    trans(path, savepath)
