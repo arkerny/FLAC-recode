c547a0ee6437cfbf6f4959b458682324e5f6f3d1
arkerny <arkerny@gmail.com>
2023-09-12 19:35:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os
+import subprocess
+import time
 
 def trans(path):
     pathDir = os.listdir(path)
+    log = open('log.txt', 'w', encoding = 'utf-8')
+
+    log.write('$ Time: ' + time.strftime("%a %b %d %H:%M %Y", time.localtime()) + '\n')
+    log.write('---------- Start ----------' + '\n')
+
     for allDir in pathDir:
-        saveDir = allDir.replace('.flac', '.wav')
-        saveDir = saveDir.replace('.mp3', '.wav')
-        cmd = 'ffmpeg -i ' + path + allDir + ' ' + path + saveDir
-        print(cmd)
-        try:
-            os.system(cmd)
-            print('Done')
-        except:
-            print('Fail!')
+        saveDir = allDir.replace('.flac', '_recode.flac')
+        saveDir = saveDir.replace('.mp3', '_recode.flac')
+        cmd = 'ffmpeg -y -i "' + path + allDir + '" "' + path + '00_' + saveDir + '"'
+        log.write('$ Time: ' + time.strftime("%a %b %d %H:%M %Y", time.localtime()) + '\n')
+        log.write('# ' + cmd + ' Start!' + '\n')
+        ret = subprocess.getoutput(cmd)
+        log.write(ret + '\n')
+        log.write('# ' + allDir + ' Done!' + '\n')
+        log.write('---------- Next ----------' + '\n')
+
+    log.write('$ Time: ' + time.strftime("%a %b %d %H:%M %Y", time.localtime()) + '\n')
+    log.write('---------- End ----------' + '\n')
+    print("Done!")
 
 if __name__ == '__main__':
     path = input('Please input dir path: ')
